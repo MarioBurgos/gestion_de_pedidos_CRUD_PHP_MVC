6d6c205a8c9cb12e523973c3a11fc604f68c3719
Mario Burgos <mario.burpi@gmail.com>
2022-10-04 12:43:03 +0200

    detalle productos/clientes

--- view/detalle_cliente.php
+++ view/detalle_cliente.php
@@ -1,1 +1,47 @@
-detalle
+<div class="section-two w-100">
+    <div class="container-fluid">
+        <div class="col-md-12 text-right">
+            <a href="index.php?controller=cliente&action=edit" class="btn btn-primary">Nuevo cliente</a>
+            <hr />
+        </div>
+        <div class="d-flex flex-wrap justify-content-between">
+            <?php
+            if (count($dataToView["data"]) > 0) {
+            $cliente = $dataToView["data"];
+            ?>
+                <div class="col-md-12 mx-auto mb-3 px-2">
+                    <div class="card-body bg-light shadow rounded">
+                        <h5 class="card-title">
+                            <?php echo '<b>' . $cliente['dni'] . '</b>' ?>
+                            <small><span class="float-end">id: <?php echo $cliente['id']; ?></span></small>
+                        </h5>
+                        <div class="card-text">
+                            <?php echo $cliente['apellido1'] . ' ' . $cliente['apellido2'] . ', ' . $cliente['nombre']; ?>
+                        </div>
+                        <hr class="my-2" />
+                        <div class="text-end">
+                            <a href="index.php?controller=cliente&action=confirmDelete&id=<?= $cliente['id']; ?>" class="btn btn-sm btn-danger mx-1 ">Eliminar</a>
+                            <a href="index.php?controller=cliente&action=edit&id=<?= $cliente['id']; ?>" class="btn btn-sm btn-primary mx-1 ">Editar</a>
+                        </div>
+                    </div>
+                </div>
+            <?php
+            } else {
+            ?>
+                <div class="alert alert-info">
+                    Ha surgido un error al mostrar este cliente.
+                </div>
+            <?php
+            }
+            ?>
+        </div>
+    </div>
+    <!--  
+    <a href='#' class='scroll-top'>
+    <svg height='35' viewBox='0 0 24 24' width='35' xmlns='http://www.w3.org/2000/svg'>
+       <path d='M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z'/>
+       <path d='M0 0h24v24H0z' fill='none'/>
+    </svg>
+    </a>
+-->
+</div>
--- view/detalle_producto.php
+++ view/detalle_producto.php
@@ -1,1 +1,46 @@
-detail
+<div class="section-two w-100">
+    <div class="container-fluid">
+        <div class="col-md-12 text-right">
+            <a href="index.php?controller=cliente&action=edit" class="btn btn-primary">Nuevo producto</a>
+            <hr />
+        </div>
+        <div>
+            <?php
+            if ($dataToView["data"]) {
+                $producto = $dataToView["data"];
+            ?>
+                <div class="col-md-12 mx-auto mb-3 px-2">
+                    <div class="card-body border border-secondary bg-light rounded">
+                        <h5 class="card-title"> Código: <?php echo $producto['codigo']; ?><small> <span class="float-end">id: <?php echo $producto['id']; ?></span></small> </h5>
+                        <hr class="my-2" />
+                        <div class="card-text">
+                            <?php echo $producto['nombre']  ?>
+                            <?php echo '<br><b> (' . $producto['precio'] . '€)</b>' ?>
+                            <?php echo '<br><br><i>' . $producto['descripcion'] . '</i>' ?>
+                        </div>
+                        <div class="text-end">
+                            <a href="index.php?controller=producto&action=confirmDelete&id=<?= $producto['id']; ?>" class="btn btn-sm btn-danger mx-1">Eliminar</a>
+                            <a href="index.php?controller=producto&action=edit&id=<?= $producto['id']; ?>" class="btn btn-sm btn-primary mx-1">Editar</a>
+                        </div>
+                    </div>
+                </div>
+            <?php
+            } else {
+            ?>
+                <div class="alert alert-info">
+                Ha surgido un error al mostrar este producto.
+                </div>
+            <?php
+            }
+            ?>
+        </div>
+    </div>
+    <!--  
+    <a href='#' class='scroll-top'>
+    <svg height='35' viewBox='0 0 24 24' width='35' xmlns='http://www.w3.org/2000/svg'>
+       <path d='M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z'/>
+       <path d='M0 0h24v24H0z' fill='none'/>
+    </svg>
+    </a>
+-->
+</div>
